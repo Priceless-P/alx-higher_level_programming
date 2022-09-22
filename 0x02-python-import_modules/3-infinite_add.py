af6d5fcf9206b3c2e25b684f24359e1c1d52a53d
Priceless-P <prisca.priceless11@gmail.com>
2022-09-22 23:48:26 +0100

    Correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,10 @@
 # 3_infinite.py
 
 if __name__ == "__main__":
-    """ Prints the result of the addition of all arguments """
+    """Prints the result of the addition of 
+    all arguments. """
     import sys
     count = 0
-    for i in range (len(sys.argv) - 1):
+    for i in range(len(sys.argv) - 1):
         count += int(sys.argv[i + 1])
         print("{}".format(count))
