27e36f208773273a18d3a67ab2b780a9a665d66b
Priceless-P <prisca.priceless11@gmail.com>
2022-09-26 17:19:14 +0100

    Correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,10 @@
 
 def no_c(my_string):
     """Removes all characters c and C from a string."""
-    str_copy = my_string.translate({ord(x): None for x in 'cC'})
+    str_copy = ""
+    for x in my_string:
+        if x == "c" or x == "C":
+            continue
+        else:
+            str_copy += 1
     return str_copy
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,12 @@
 
 def multiple_returns(sentence):
     """Returns a tuple with the length of a string and its first character"""
-
-    tuple_length = len(sentence);
-    first_character = sentence[0];
+    if len(sentence) == 0:
+        first_character = None
+        tuple_length = 0
+    else:
+        sentence = list(sentence)
+        tuple_length = len(sentence);
+        first_character = sentence[0];
 
     return tuple_length, first_character
