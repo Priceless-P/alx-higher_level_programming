258ccd1e0335914c5c1a6808703563cf920bbc84
Priceless-P <prisca.priceless11@gmail.com>
2022-09-26 17:35:07 +0100

    Correction

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,38 +1,76 @@
+/*
+ * File: 13-is_palindrome.c
+ */
+
 #include "lists.h"
-#include <stdlib.h>
+
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
 /**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: the first node of a linked list
- * Return: 0 if it is not a palinfrome and 1 if it is a palindrome
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp, *new;
-	int i, count = 1;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-	temp = new = (*head);
-	if ((*head)->next == NULL || (*head) == NULL)
-	{
-		return  (1);
-	}
-	while (temp->next != NULL)
-	{
-		count++;
-		temp = temp->next;
-	}
-	int array[count];
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-	for (i = 0; i < count; i++)
+	tmp = *head;
+	while (tmp)
 	{
-		array[i] = new->n;
-		new = new->next;
+		size++;
+		tmp = tmp->next;
 	}
-	for (i = 0; i < (count / 2); i++)
+
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
 	{
-		if (array[i] != array[count - 1 - i])
-		{
+		if (tmp->n != rev->n)
 			return (0);
-		}
+		tmp = tmp->next;
+		rev = rev->next;
 	}
+	reverse_listint(&mid);
+
 	return (1);
 }
