8b38ac8ca87278b44df7f3a76ad4a32f71dff888
Priceless-P <prisca.priceless11@gmail.com>
2022-09-29 00:27:07 +0100

    Correction

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,7 +4,5 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    ordered = list(a_dictionary.keys())
-    ordered.sort()
-    for x in ordered:
-        print("{}:{}".format(x, a_dictionary.get(x)))
+    for keys in sorted(a_dictionary.keys()):
+        print("{:s}: {}".format(keys, a_dictionary[keys]))
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -4,6 +4,6 @@
 
 
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary.get(key) is not None:
+    if key in a_dictionary:
         del a_dictionary[key]
     return a_dictionary
