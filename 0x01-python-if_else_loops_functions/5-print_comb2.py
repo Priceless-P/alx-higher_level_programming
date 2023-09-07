ef212d6b8f9a81ed6646335e8af7d417edb55d09
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-09-07 16:11:03 +0100

    correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 """Prints numbers from 0 to 99"""
 for i in range(0, 100):
-    print("{}{}".format(0, i) if i < 10 else "{}".format(i), sep=", ")
+    if i < 99:
+        print("{}{}".format(0, i) if i < 10 else "{}".format(i), end=", ")
+    else:
+        print("{}".format(i))
