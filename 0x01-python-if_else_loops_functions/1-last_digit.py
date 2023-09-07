39e55383d780038ffd811afb9353c792075bb14d
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-09-07 16:18:42 +0100

    correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,23 @@ number each time it is executed and  print the last
 digit of the number stored in the variable number."""
 import random
 number = random.randint(-10000, 10000)
-last_num = int(str(number)[-1])
+last_num = number % 10
 if last_num > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_num))
-elif last_num == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_num))
-elif last_num < 6 and last_num != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_num))
+    if number < 0:
+        print(
+            "Last digit of {} is {} and is greater than {}"
+            .format(-number, 10 - last_num, 5))
+    else:
+        print("Last digit of {} is {} and is greater than 5"
+              .format(number, last_num))
+if last_num == 0:
+    print("Last digit of {} is {} and is 0"
+          .format(number, last_num))
+if last_num < 6 and last_num != 0:
+    if number < 0:
+        print(
+            "Last digit of {} is {} and is less than {} and not {}"
+            .format(number, -(10 - last_num), 6, 0))
+    else:
+        print("Last digit of {} is {} and is less than 6 and not 0"
+              .format(number, last_num))
