85ca86258a6fc986077db46421eb736ac61b5ef0
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-09-07 16:06:46 +0100

    correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 """Prints numbers from 0 to 99"""
 for i in range(0, 100):
-    print("{}{}".format(0, i) if i < 10 else "{}".format(i), end=", ")
+    print("{}{}".format(0, i) if i < 10 else "{}".format(i), sep=", ")
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+"""Prints all possible different combinations of two digits"""
