cb660899c5748b51dc07a38a60fe08fab5080e33
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-09-13 13:30:02 +0100

    fix error

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,11 @@
-#!/usr/bin/pyhon3
+#!/usr/bin/python3
 
 def weight_average(my_list=[]):
     """Returns the weighted average of all
     integers tuple (<score>, <weight>)"""
     if not isinstance(my_list, list) or len(my_list) == 0:
         return 0
-        
+
     total_score = 0
     total_weight = 0
     for scores in my_list:
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,4 @@
-#!/usr/python3
+#!/usr/bin/python3
 
 def uniq_add(my_list=[]):
     """Adds all unique integers in a list (only once for each integer)."""
