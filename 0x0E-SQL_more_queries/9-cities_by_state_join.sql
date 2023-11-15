37e86992be354c1b1c129adb0dcd40f47f349069
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-11-15 23:57:52 +0100

    correction

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,9 @@
 -- Lists all cities contained in the
---database hbtn_0d_usa
+-- database hbtn_0d_usa
 
 SELECT c.`id`, c.`name`, s.`name`
     FROM `cities` AS c
         INNER JOIN `states` AS s
         ON c.`state_id` = s.`id`
     ORDER BY c.`id`;
+
