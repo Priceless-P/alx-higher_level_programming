360f30669176476dbda1ada3048f3d370434fda5
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-12-12 22:30:33 +0100

    correction

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,6 +6,4 @@ const [, , file1, file2, destFile] = process.argv;
 const contentsFile1 = fs.readFileSync(file1, 'utf8');
 const contentsFile2 = fs.readFileSync(file2, 'utf8');
 
-const bothContents = `${contentsFile1.trim()}\n${contentsFile2.trim()}\n`;
-
-fs.writeFileSync(destFile, bothContents);
+fs.writeFileSync(destFile, contentsFile1 + contentsFile2);
