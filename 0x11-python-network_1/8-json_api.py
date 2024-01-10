9cdc9a04deb29b7a365a17a014cdfd1f97ac4d52
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-01-10 22:57:21 +0100

    correction

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,10 +10,7 @@ import requests
 
 if __name__ == '__main__':
     url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) == 2:
-        q = sys.argv[1]
-    else:
-        q = ""
+    q = "" if len(sys.argv) == 1 else sys.argv[1]
     data = {'q': q}
 
     response = requests.post(url, data=data)
