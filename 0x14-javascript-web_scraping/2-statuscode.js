03dad9ce55a05ef204150794c5cc5ad66c3b106a
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-01-30 14:44:03 +0100

    correction

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,8 +7,10 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, (_, response) => {
-  {
-    console.log('code: ', response.statusCode);
+request.get(url, (err, response) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('code: ' + response.statusCode);
   }
 });
